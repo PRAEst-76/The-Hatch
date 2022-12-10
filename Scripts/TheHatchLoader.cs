aafeba32236523b264ee2133e8b39fd7bfcafc79
Peter Adams <43264557+PRAEst-76@users.noreply.github.com>
2022-12-10 23:47:16 +0200

    Latest fiddlings and basement door.
    New door model for alternative basement access in addition to the hatch plus latest (non working) script.

--- Scripts/TheHatchLoader.cs
+++ Scripts/TheHatchLoader.cs
@@ -1,67 +1,43 @@
 using UnityEngine;
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
 using DaggerfallWorkshop.Game;
+//using DaggerfallWorkshop.Game.UserInterface;
+using static DaggerfallWorkshop.Game.PlayerActivate;
+//using static DaggerfallWorkshop.DaggerfallLadder;
 using DaggerfallWorkshop.Game.Utility.ModSupport;   //required for modding features
 
-namespace LocationLoader
+namespace DaggerfallWorkshop.Game
 {
-    public static class HatchModLoader
+    public class HatchModLoader : MonoBehaviour
     {
-        public static Mod mod { get; private set; }
-        public static GameObject modObject { get; private set; }
+        public static Mod mod;
+        public static GameObject modObject;
 
-        [Invoke(StateManager.StateTypes.Start, 0)]
         public static void Init(InitParams initParams)
         {
-            // Get mod
             mod = initParams.Mod;
+            modObject = new GameObject(mod.Title);
+            modObject.AddComponent<HatchModLoader>();
 
-            modObject = new GameObject("LocationLoader");
-            modObject.AddComponent<LocationLoader>();
-            mod.SaveDataInterface = modObject.AddComponent<LocationSaveDataInterface>();
-            modObject.AddComponent<LocationResourceManager>();
+            PlayerActivate.RegisterCustomActivation(mod, 41410, 41411, OnHatchActivated);
+
             mod.IsReady = true;
-
-            const int hatchModelId = 41410;
-            PlayerActivate.RegisterCustomActivation(mod, hatchModelId, OnHatchActivated);
         }
 
-        static void OnHatchActivated(RaycastHit hit)
+        private static void OnHatchActivated(RaycastHit hit)
         {
- 
-            Transform ladderTransform = hit.transform;
-            Transform prefabTransform = ladderTransform.parent;
-            GameObject prefabObject = prefabTransform.gameObject;
-            LocationData data = prefabObject.GetComponent<LocationData>();
-
-            PlayerMotor playerMotor = GameManager.Instance.PlayerMotor;
-            bool foundBottom = data.FindClosestMarker(EditorMarkerTypes.LadderBottom, playerMotor.transform.position, out Vector3 bottomMarker);
-            bool foundTop = data.FindClosestMarker(EditorMarkerTypes.LadderTop, playerMotor.transform.position, out Vector3 topMarker);
-
-            Vector2 ladderPlanarPos = new Vector2(ladderTransform.position.x, ladderTransform.position.z);
-            Vector2 bottomMarkerPlanarPos = new Vector2(bottomMarker.x, bottomMarker.z);
-            Vector2 topMarkerPlanarPos = new Vector2(topMarker.x, topMarker.z);
-
-            float bottomPlanarDistance = Vector2.Distance(ladderPlanarPos, bottomMarkerPlanarPos);
-            float topPlanarDistance = Vector2.Distance(ladderPlanarPos, topMarkerPlanarPos);
-
-            const float MaxMarkerDistance = PlayerActivate.DefaultActivationDistance * 2;
-            foundBottom = foundBottom && bottomPlanarDistance < MaxMarkerDistance;
-            foundTop = foundTop && topPlanarDistance < MaxMarkerDistance;
-
-            float bottomDistance = Vector3.Distance(playerMotor.transform.position, bottomMarker);
-            float topDistance = Vector3.Distance(playerMotor.transform.position, topMarker);
-
-            // Teleport to top marker
-            if (foundTop && (!foundBottom || topDistance > bottomDistance))
-            {
-                playerMotor.transform.position = topMarker;
-                playerMotor.FixStanding();
-            }
-            else if (foundBottom)
-            {
-                playerMotor.transform.position = bottomMarker;
-                playerMotor.FixStanding();
-            }
+            DaggerfallLadder ladder = hit.transform.GetComponent<DaggerfallLadder>();
+                if (hit.distance > DefaultActivationDistance)
+                {
+                    DaggerfallUI.SetMidScreenText(TextManager.Instance.GetLocalizedText("youAreTooFarAway"));
+                    return;
+                }
+                else
+                {
+                    ladder.ClimbLadder();
+                }
         }
     }
 }
